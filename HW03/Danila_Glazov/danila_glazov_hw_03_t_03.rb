6bd7982aa6ad1498351ecef68f8366a25ed3f301
Danila Glazov <danikglazov@gmail.com>
2021-04-27 22:39:07 +0300

    [HW-03][TASK-03] Implement method to calculate duration between two actions

---
+++ HW03/Danila_Glazov/danila_glazov_hw_03_t_03.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+require 'time'
+def calc_duration(actions)
+  duration_results = []
+  prev_datetime = ''
+
+  actions.split("\n").each do |action|
+    current_datetime = action.match(/^\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2}:\d{1,2}\.\d{1}/)[0]
+    unless prev_datetime.empty?
+      duration_results << (Time.parse(current_datetime) - Time.parse(prev_datetime)).round(1).to_s
+    end
+    prev_datetime = current_datetime
+  end
+
+  duration_results.empty? ? '0' : duration_results
+end
+
+actions = ''
+puts "Input logs or type \'end\' to continue:"
+while (text = gets) != "end\n"
+  actions += text
+end
+
+p calc_duration(actions)
