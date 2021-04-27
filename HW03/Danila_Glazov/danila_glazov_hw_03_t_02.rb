1a88f6bf83375e516558fc5e5ac03eb1578498a6
Danila Glazov <danikglazov@gmail.com>
2021-04-27 15:49:30 +0300

    [HW-03][TASK-02] Implement method to format input logs

---
+++ HW03/Danila_Glazov/danila_glazov_hw_03_t_02.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: false
+
+def format_logs(logs_str)
+  valid_format = /^\d{2}\.\d{1}\.\d{3}\.\d{3}\s-\s-\s.+$/
+  formated_logs = []
+
+  logs_str.split("\n").each do |log|
+    next unless log.match?(valid_format)
+
+    data = log.match(%r{\d{1,2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}})
+    from = log.match(/^\d{1,2}\.\d{1}\.\d{1,3}\.\d{1,3}/)
+    to = log.match(%r{"\w+\s(.*)\s\w+/\d{1}\.\d{1}})[1].upcase
+
+    formated_logs << "#{data} FROM: #{from} TO: #{to}"
+  end
+
+  formated_logs
+end
+
+logs = ''
+
+puts "Input logs or type \'end\' to continue:"
+while (text = gets) != "end\n"
+  logs += text
+end
+
+p format_logs(logs)
